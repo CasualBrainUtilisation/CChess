7ac26e7cccf6343a1d3bff12e38d1b788a834850
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2024-01-01 15:44:12 +0100

    getHash and GetPieceAt

--- hf/CCBoard.c
+++ hf/CCBoard.c
@@ -14,5 +14,21 @@ typedef struct Vector2
 typedef struct Board
 {
     Piece *pAllPieces; // A simple pointer used as array that contains all the chessPieces present on Board
-    Piece *piecesHashTable[64]; // A hashTable that uses the pos of a piece as key an stores the pointer to the piece as value
+    Piece *piecesHashTable[64]; // A hashTable that uses the pos of a piece as key an stores the pointer to the piece as value, notice it actually stores the address of the pieces, which are first defined in the pAllPieces pointerarray
+    // This hashtable comes in handy, cuz it improves performance when checking for a piece at a boardPos
 } Board;
+
+/* Hashtable functions */
+// Simple function that'll return the hash of inputed pos
+// ALL poses have a unique hash
+int getHash(Pos *pos)
+{
+    return pos->X * 8 - pos->Y;
+}
+
+// Returns the piece at given pos on the given board using its piecesHashTable
+// If there is no piece at the given pos, returned piece will be of type NONE
+Piece *GetPieceAtPos(Pos *pos, Board *board)
+{
+    return board->piecesHashTable[getHash(&pos)];
+}
