e301cbc9031db6c375276a276178e6bcf521c168
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2024-01-01 12:54:32 +0100

    CCBoard Board struct

--- hf/CCBoard.c
+++ hf/CCBoard.c
@@ -1,6 +1,7 @@
+#include "CCPieces.h"
+
 /* Contains the ChessBoard and Pos struct (which represents a pos on the chessboard) and also all the functions that impact or require the ChessBoard struct */
 
-/* Boards Info */
 // Pos represents a position on the chessBoard
 // This can be imagined like a cordinate system on the board
 typedef struct Vector2
@@ -8,3 +9,10 @@ typedef struct Vector2
     int X; // The X represents the files of the ChessBoard (The numbers 1 - 8 (although we use the scope of 0-7))
     int Y; // The Y represents the ranks of the ChessBoard (The letters a - h (with 0-7))
 } Pos;
+
+// Struct containing info about a chessBoard aka thee pieces placed on top
+typedef struct Board
+{
+    Piece *pAllPieces; // A simple pointer used as array that contains all the chessPieces present on Board
+    Piece *piecesHashTable[64]; // A hashTable that uses the pos of a piece as key an stores the pointer to the piece as value
+} Board;
